2ec6491af98659a17b9265c7458aaa5795bd95d7
Serse Dio Re <32899592+SerseDioRe@users.noreply.github.com>
2021-02-16 17:17:23 +0100

    updated

--- SHBypass/classes.h
+++ SHBypass/classes.h
@@ -8,11 +8,3 @@ public:
 	char pad_0001[127];
 };
 static_assert(sizeof(GtaCoreFiveDll) == 0x80);
-
-class AdhesiveDll
-{
-public:
-	int32_t value;
-	char pad_0004[124];
-};
-static_assert(sizeof(AdhesiveDll) == 0x80);
